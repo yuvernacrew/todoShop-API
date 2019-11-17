8baeafba10e113b215c5dbfc199360632fc6a0e3
加藤　優奈 <yuvernacrew@gmail.com>
2019-11-17 14:06:22 +0900

    CSRF対策

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::API
+  # CSRF対策
+  protect_from_forgery with: :null_session
+
   # TODO: 何を継承してるのかさっぱりわからん。AbstractControllerって何.
   include AbstractController::Translation
 
